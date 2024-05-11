cf4382969db5ac8c2902af0690456cb845ee9668
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-11 22:25:38 +0000

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,30 +1,36 @@
-import { redis } from 'redis';
+import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-	constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => {
-			console.error('Error connecting to Redis:', err);
-		});
-	}
-	isAlive() {
-		return this.client.connected;
-	}
-	async get(key, value, duration) {
-		const gt = promisify(this.client.get).bind(this.client);
-		return gt(key);
-	}
-	async set(key, value, duration) {
-		const st = promisify(this.client.set).bind(this.client);
-		return st(key, value, "EX", duration);
-	}
-	async del(key) {
-		const dlt = promisify(this.client.del).bind(this.client);
-		return dlt(key);
-	}
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error('Error connecting to Redis:', err);
+    });
+  }
 
+  isAlive () {
+    if (this.client.connected) {
+      return true;
+    } 
+    return false;
+    
+  }
+
+  async get (key, value, duration) {
+    const gt = promisify(this.client.get).bind(this.client);
+    return gt(key);
+  }
+
+  async set (key, value, duration) {
+    const st = promisify(this.client.set).bind(this.client);
+    return st(key, value, 'EX', duration);
+  }
+
+  async del (key) {
+    const dlt = promisify(this.client.del).bind(this.client);
+    return dlt(key);
+  }
 }
-const redisClient = RedisClient();
+const redisClient = new RedisClient();
 export default redisClient;
-
