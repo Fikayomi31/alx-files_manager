4faa0097298ef4d5201da496d642aa80d6c59077
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-13 00:32:55 +0000

    mongodb utils

--- utils/db.js
+++ utils/db.js
@@ -1,34 +1,33 @@
 import { MongoClient } from 'mongodb';
-/**
-* - class DBClient is our handler for the database
- */
+
+
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || '127.0.0.1';
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
-    this.cli = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
-    this.cli.connect();
+    this.client = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
+    this.client.connect();
   }
 
   isAlive() {
-    return this.cli.isConnected();
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    return this.cli.db().collection('users').countDocuments();
+    return this.client.db().collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.cli.db().collection('files').countDocuments();
+    return this.client.db().collection('files').countDocuments();
   }
 
   async files() {
-    return this.cli.db().collection('files');
+    return this.client.db().collection('files');
   }
 
   async users() {
-    return this.cli.db().collection('users');
+    return this.client.db().collection('users');
   }
 }
 
