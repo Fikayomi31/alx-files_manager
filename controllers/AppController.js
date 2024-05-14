ef86d5bdbda58384a4274b47f5b19278e8fc42ae
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-13 19:09:56 -0700

    2. First API

---
+++ controllers/AppController.js
@@ -0,0 +1,21 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+    static async getStatus(request, response) {
+        response.status(200).json({
+            redis: await redisClient.isAlive(),
+            db: await dbClient.isAlive()
+        });
+    }
+    static async getStats(request, response) {
+        const userNum = await dbClient.nbUsers();
+        const fileNum = await dbClient.nbFiles();
+        reponse.statu(200).json({
+            users: userNum,
+            files: fileNum
+        });
+    }
+}
+
+module.exports = AppController;
